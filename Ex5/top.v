4e8b8ed4b86c3bc63f3ac6055c91424ef8521c23
Cloud User <centos@ip-172-31-38-19.eu-west-2.compute.internal>
2021-05-25 15:06:09 +0000

    Incomplete Solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -15,11 +15,18 @@
 
 `timescale 1ns / 100ps
 
-module ac(input clk, reg [4:0]temp,
-output heat, cool);
+module ac(input reg clk, reg [4:0]temp,
+output reg heat, reg cool);
 
 //Standard operating conditions
-
-
+always @(posedge clk)
+	begin
+	assign heat = (temp<18)?1: (temp>=20)?0: heat;
+	assign cool = (temp<=20)?0: (temp>22)?1: cool;
 //Illegal state - both heat and cool on
 //resolve by setting both to zero and letting system decide which (if any) to reactivate based on temp
+	if (cool==1 & heat==1)
+		assign cool = 0;
+		assign heat = 0;
+	end
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -8,3 +8,65 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg heat;
+reg cool;
+reg err;
+wire [4:0]temp;
+
+//Clock generation
+initial
+begin
+clk = 1'b0;
+	forever
+        #(CLK_PERIOD/2) clk=~clk;
+end
+//start at temp = 0:
+assign temp = 5'b00000;
+initial
+begin
+heat = 0;
+cool = 0;
+forever begin
+	#10 if ((heat == 1 && temp > 20) || (cool == 1 && temp < 20) || (heat == 1 && cool == 1))
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+
+//Trial response to heat = cool = 1:
+
+	heat = 1; //updating heat and cool
+	cool = 1;
+	#10 if ((heat == 1 && temp > 20) || (cool == 1 && temp < 20) || (heat == 1 && cool == 1)) //confirming no illegal states in subsequenct clock cycles
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+
+	end
+end
+
+//repeating but start at mid temp (20)
+//Todo: Finish test, check for success
+initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+	if (err==1)
+	  $display("***TEST FAILED! :( ***");
+        $finish;
+      end
+
+//Instantiating ac top:
+ac top(clk,temp,heat,cool);
+
+endmodule
