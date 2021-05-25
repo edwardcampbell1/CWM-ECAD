e55fc11eeae4b2b877f0e26512c9311c17970d59
Cloud User <centos@ip-172-31-38-19.eu-west-2.compute.internal>
2021-05-25 11:27:22 +0000

    Complete Solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Edward Campbell
+// Date: 25/05/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,45 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module lights(input rst,clk,button,
+output reg [2:0]colour);
+
+//Todo: add user logic
+initial begin
+	colour = 3'b001;
+end
+
+always @(posedge clk)
+	begin
+	if (button == 0 && (colour == 3'b111 || colour == 3'b000))
+		colour = 3'b001;
+	else if (button == 0)
+		colour = colour;
+	else if ((button == 1) && (colour != 3'b111))
+		colour = colour + 1;
+	else
+		colour = 3'b001;
+	end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,92 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Edward Campbell
+// Date: 25/05/21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg rst;
+reg button;
+wire [2:0]colour;
+reg err;
+
+//Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Trial for colour start at 111 button = 0:
+initial begin
+rst=0;
+button=0;
+//colour=111;
+err=0;
+forever begin
+	#10 if (colour==3'b111 | colour==3'b000)
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+	button=1; //Now testing for button being held - should cycle but never have colour 000 or 111
+	#10 if (colour==3'b111 | colour==3'b000)
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+	end
+end
+
+
+
+//Todo: Finish test, check for success
+initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+	if (err==1)
+	  $display("***TEST FAILED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate lights module
+lights top(clk,rst,button,colour);
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
